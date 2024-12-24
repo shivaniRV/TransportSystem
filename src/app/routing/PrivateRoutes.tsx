6416b3232f9943325e9ade286f82c3a565014dd1
shivaniRV <2020bcs140@sggs.ac.in>
2024-12-24 18:11:29 +0530

    updated route

--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -7,7 +7,7 @@ import { MenuTestPage } from "../pages/MenuTestPage";
 import { getCSSVariableValue } from "../../_metronic/assets/ts/_utils";
 import { WithChildren } from "../../_metronic/helpers";
 import BuilderPageWrapper from "../pages/layout-builder/BuilderPageWrapper";
-import HomeMain from "../pages/AirTransport/Homepage/HomeMain";
+import HomeMain from "../pages/WaterTransport/Homepage/HomeMain";
 const PrivateRoutes = () => {
   const ProfilePage = lazy(() => import("../modules/profile/ProfilePage"));
   const WizardsPage = lazy(() => import("../modules/wizards/WizardsPage"));
