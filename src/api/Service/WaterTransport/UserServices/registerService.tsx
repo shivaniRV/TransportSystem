5def97d881ca6fed7e46a6c301efe60e1dae65b5
gayatri <patwegayatri5143@gmail.com>
2025-01-03 14:32:50 +0530

    changed interface

---
+++ src/api/Service/WaterTransport/UserServices/registerService.tsx
@@ -0,0 +1,23 @@
+// import { useNavigate } from 'react-router-dom';
+
+
+
+// const API_URL = import.meta.env.VITE_APP_API_URL;
+// const Registration = () => {
+//     const navigate = useNavigate();
+
+//     const handleSubmit = async (values) => {
+//         try {
+            
+//             await axios.post(API_URL'/api/v1/user/signup', values);
+//             // Redirect to login page after successful registration
+//             navigate('/login');
+//         } catch (error) {
+//             console.error('Error during registration', error);
+//         }
+//     };
+
+//     return (
+//         // Your form here
+//     );
+// };
